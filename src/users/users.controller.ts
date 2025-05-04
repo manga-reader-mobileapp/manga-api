b555b532a15cffbfbdaa0c190de010b9e4f3ed3c
zapdesign <giuseppegabriel4@gmail.com>
2025-05-04 16:03:45 -0300

    feat: implement user creation endpoint and service method

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { IsPublic } from 'src/auth/decorators/is-public.decorator';
+import { JwtAuthGuard } from 'src/auth/guard/jwt-auth.guard';
+import { UsersService } from './users.service';
 
+@UseGuards(JwtAuthGuard)
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @IsPublic()
+  @Post('/create')
+  async createUser(
+    @Body() body: { email: string; name: string; password: string },
+  ) {
+    return await this.usersService.createUser(
+      body.email,
+      body.name,
+      body.password,
+    );
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -34,4 +34,34 @@ export class UsersService {
 
     return;
   }
+
+  async createUser(email: string, name: string, password: string) {
+    const hashedPass = await hash(password, 10);
+
+    const user = await this.prisma.user.create({
+      data: {
+        email,
+        name,
+        password: hashedPass,
+        isActive: false,
+      },
+      select: {
+        id: true,
+      },
+    });
+
+    await this.prisma.category.create({
+      data: {
+        name: 'Default',
+        orderKanban: 0,
+        user: {
+          connect: {
+            id: user.id,
+          },
+        },
+      },
+    });
+
+    return;
+  }
 }
